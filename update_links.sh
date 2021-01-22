b0801fe61cf5060e819e9ec5fc512618ed19c986
Matthew Maciejewski <mmaciej2@jhu.edu>
2021-01-22 12:18:40 -0500

    updated CV links

--- update_links.sh
+++ update_links.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sed -i -e 's/cv\.pdf"/cv.pdf" target="_blank"/g' index.html
+for file in index.html */index.html; do
+  sed -i -e 's/cv\.pdf"/cv.pdf" target="_blank"/g' $file
+done
 sed -i -e 's/\[<a href=\(.*\)>\(.*\)<\/a>\]/[<a href=\1 target="_blank">\2<\/a>]/g' publications/index.html
 sed -i -e 's/>here</ target="_blank">here</g' about/index.html
