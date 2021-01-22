1340874bbb6250ee104ad00367ef6dde507f77f6
Matthew Maciejewski <mmaciej2@jhu.edu>
2021-01-22 12:21:21 -0500

    updated links

--- update_links.sh
+++ update_links.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 for file in index.html */index.html; do
   sed -i -e 's/cv\.pdf"/cv.pdf" target="_blank"/g' $file
+  sed -i -e 's/Linkedin">/Linkedin" target="_blank">/g' $file
+  sed -i -e 's/GitHub">/GitHub" target="_blank">/g' $file
 done
 sed -i -e 's/\[<a href=\(.*\)>\(.*\)<\/a>\]/[<a href=\1 target="_blank">\2<\/a>]/g' publications/index.html
 sed -i -e 's/>here</ target="_blank">here</g' about/index.html
